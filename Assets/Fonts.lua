7a397473c8b0c1f007980bbd91b37540a771c72c
lcsezX <linconlcarter@gmail.com>
2026-05-24 14:27:59 -0400

    Add Fonts.lua to define font styles

---
+++ Assets/Fonts.lua
@@ -0,0 +1,10 @@
+-- Assets/Fonts.lua
+local Fonts = {}
+
+Fonts.Title = Enum.Font.GothamBlack
+Fonts.Subtitle = Enum.Font.GothamBold
+Fonts.Text = Enum.Font.Gotham
+Fonts.Button = Enum.Font.GothamBold
+Fonts.Code = Enum.Font.Code
+
+return Fonts
